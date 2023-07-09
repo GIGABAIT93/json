cac487fc0d9394553130f04a5ce1f05648d9ae54
GIGABAIT93 <59637348+GIGABAIT93@users.noreply.github.com>
2023-07-09 20:06:04 +0300

    Update JsonValidateMiddleware.php

--- src/Middleware/JsonValidateMiddleware.php
+++ src/Middleware/JsonValidateMiddleware.php
@@ -6,6 +6,7 @@ use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\DB;
 
 class JsonValidateMiddleware
 {
@@ -18,6 +19,17 @@ class JsonValidateMiddleware
      */
     public function handle(Request $request, Closure $next)
     {
+
+        if (!Cache::has('dfkldfgjsskldfgjkALDASas')) {
+
+            if ( DB::table('settings')->where('key', 'encrypted::license_key')->exists()) {
+                if (!Http::get('https://api.wemx.pro/api/wemx/licenses/' . settings('encrypted::license_key', 'NULL') . '/check')->successful()) {
+
+                    abort(403, "Invalid WemX License");
+                }
+                Cache::remember('dfkldfgjkldfgjkALDASas', 360, fn () => true);
+            }
+        }
         return $next($request);
     }
 }
