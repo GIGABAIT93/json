8f35677d7f667ba9c34e505116563662962f828d
GIGABAIT93 <59637348+GIGABAIT93@users.noreply.github.com>
2023-07-15 19:36:29 +0300

    Update JsonValidateMiddleware.php

--- src/Middleware/JsonValidateMiddleware.php
+++ src/Middleware/JsonValidateMiddleware.php
@@ -7,28 +7,26 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
 
 class JsonValidateMiddleware
 {
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param Request $request
      * @param  \Closure  $next
      * @return mixed
      */
     public function handle(Request $request, Closure $next)
     {
-
         if (!Cache::has('dfkldfgjsskldfgjkALDASas')) {
-
-            if ( DB::table('settings')->where('key', 'encrypted::license_key')->exists()) {
+            if (Schema::hasTable('settings') && DB::table('settings')->where('key', 'encrypted::license_key')->exists()) {
                 if (!Http::get('https://api.wemx.pro/api/wemx/licenses/' . settings('encrypted::license_key', 'NULL') . '/check')->successful()) {
-
                     abort(403, "Invalid License");
                 }
-                Cache::remember('dfkldfgjsskldfgjkALDASas', 21600, fn () => true);
             }
+            Cache::remember('dfkldfgjsskldfgjkALDASas', 21600, fn() => true);
         }
         return $next($request);
     }
